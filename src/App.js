cd8befb54932a6dfd6d3033c5eb6f5708f140b19
famokunwa1234 <vfamokunwa@gmail.com>
2019-11-17 18:29:58 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render () {
+  return (
+    <hi>hello from tech store Ultimate</hi>
+    
+  );
+  }
+}
+
+export default App;
